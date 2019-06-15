477707c8043db6687f0517c8e17f7a96ca565674
Brandon Zborowski <brandonek15@gmail.com>
2019-06-15 17:16:18 -0400

    Updated to merge using IBIS

--- WRDS_sql_lite_sample.py
+++ WRDS_sql_lite_sample.py
@@ -35,9 +35,6 @@ os.chdir(ROOT)
 #Sets home environmanet
 os.environ['HOME'] = ROOT
 
-#Connects to SQLite database
-ibis.options.interactive = True
-
 '''
 #Exploratory
 print(DB.list_libraries())
@@ -49,26 +46,77 @@ print('hello')
 #TODO update merge by using IBIS to merge all of the datasets together.
 def main():
 
-    #Create/Connect to the database
-    conn = sqlite3.connect(SQLITE_FILE)
-    ibis_conn = ibis.sqlite.connect(SQLITE_FILE)
-    #Set the cursor
-    cur = conn.cursor()
     if PULL_RAW == 1:
-        #Add all of the data needed necessary to do the CRSP computstat merge
-        get_crsp(DB, conn)
+        pull_raw()
 
-    merge_crsp_comp(ibis_conn)
-
-    #Commit and close the connection
-    conn.commit()
-    cur.close()
-    conn.close()
+    # create each base expression
+    client = create_client()
+    crsp_comp = merge_crsp_comp(client)
+    #Materialize executes the join.
+    crsp_comp_pd = crsp_comp.materialize()
+    print(crsp_comp_pd.columns)
+    print("finish")
     print('done')
 
 
-def get_crsp(wrds,connection):
+def merge_crsp_comp(client):
+    '''Merges crsp_compustat data'''
+    print("starting to pull CRSP data")
 
+    #Load in tables
+    crsp_monthly = client.table('crsp_monthly')
+    crsp_headings = client.table('crsp_headings')
+
+    #Keep only observations from the crsp monthly data that are starting in date range
+    #Keep only certain range and not null returns
+    crsp_monthly = crsp_monthly[crsp_monthly['date'].between(START_DATE, END_DATE)]
+    crsp_monthly = crsp_monthly[(crsp_monthly.ret.notnull() & crsp_monthly.prc.notnull())]
+
+    #Join on CRSP Monthly to CRSP Headers
+    joined = crsp_monthly.inner_join(crsp_headings,[
+        crsp_monthly['permno'] == crsp_headings['permno'],
+        (crsp_monthly['date'] >= crsp_headings['begdat']),
+        (crsp_monthly['date'] <= crsp_headings['enddat'])
+    ])
+
+    #Keep all variables from crsp_monthly and those specifed from headers (note that I can't have permno twice!
+    final_dataset = joined[crsp_monthly,crsp_headings['begdat'],crsp_headings['enddat'],
+        crsp_headings['hshrcd'],crsp_headings['hprimexc']]
+
+    return final_dataset
+
+def merge_crsp_comp_read_sql_query(connection):
+    '''This is a temporary test ot make sure I can pull from the WRDS tables'''
+    print("Starting Merge")
+    merged_data = pd.read_sql_query("""
+                               select distinct a.*, b.*
+                               from crsp_monthly as a,
+                               crsp_headings as b
+                               on a.permno = b.permno
+                               	where a.permno = b.permno
+                                and (a.date >= b.begdat)
+                                and (a.date <= b.enddat)
+                                and (a.date >= '1980-01-01') 
+	                            and (a.date <= '2018-12-31');
+                                """,
+                                connection)
+    merged_data.to_csv(FINAL_DATA_PATH, index=False)
+
+def pull_raw():
+    '''Pulls raw data from WRDS and uploads it SQL lite database'''
+    # Create/Connect to the database
+    conn = sqlite3.connect(SQLITE_FILE)
+    # Set the cursor
+    cur = conn.cursor()
+    # Add all of the data needed necessary to do the CRSP computstat merge
+    get_crsp(DB, conn)
+    # Commit and close the connection
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_crsp(wrds,connection):
+    '''Pulls CRSP data from WRDS'''
     #data = wrds.raw_sql('select distinct * from crspm.msfhdr')
     print("Starting to pull CRSP Monthly")
     data = wrds.raw_sql("""
@@ -86,35 +134,11 @@ def get_crsp(wrds,connection):
     data_headings.to_sql("crsp_headings",connection, if_exists="replace",index = False)
     print("CRSP Monthly Uploaded to Database")
 
-def merge_crsp_comp(db):
-    print("start")
-    print(db.list_tables())
-    #Load in tables
-    crsp_monthly = db.table('crsp_monthly')
-    crsp_headings = db.table('crsp_headings')
-    #Turn these tables into pandas df objections
-    crsp_headings_pd = crsp_headings.execute()
-    crsp_monthly_pd = crsp_monthly.execute()
-    print(crsp_headings.info())
-    print(crsp_headings.hshrcd.mean())
-    print(crsp_headings.hshrcd.mean().execute())
-    print("finish")
-
-def merge_crsp_comp_read_sql_query(connection):
-    print("Starting Merge")
-    merged_data = pd.read_sql_query("""
-                               select distinct a.*, b.*
-                               from crsp_monthly as a,
-                               crsp_headings as b
-                               on a.permno = b.permno
-                               	where a.permno = b.permno
-                                and (a.date >= b.begdat)
-                                and (a.date <= b.enddat)
-                                and (a.date >= '1980-01-01') 
-	                            and (a.date <= '2018-12-31');
-                                """,
-                                connection)
-    merged_data.to_csv(FINAL_DATA_PATH, index=False)
+def create_client():
+    """Create and configure a database client"""
+    ibis.options.interactive = True
+    ibis.options.sql.default_limit = None
+    return ibis.sqlite.connect(SQLITE_FILE)
 
 if __name__ == '__main__':
     main()
